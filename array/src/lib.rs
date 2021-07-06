9b1a293af4ce191d02170fb868f35e2ff9e765da
OhBonsai <letbonsaibe@gmail.com>
2021-07-06 13:30:07 +0800

    q189

--- array/src/lib.rs
+++ array/src/lib.rs
@@ -1,3 +1,4 @@
 mod q34;
 mod q704;
 mod q35;
+mod q189;
---
+++ array/src/q189.rs
@@ -0,0 +1,40 @@
+struct Solution ();
+
+impl Solution {
+
+    pub fn rotate1(nums: &mut Vec<i32>, k: i32) {
+        let n = nums.len();
+        let mut result = nums.iter().cloned().collect::<Vec<_>>();
+
+        for (i, v) in result.iter().enumerate() {
+            if i < n-k as usize {
+                nums[k as usize + i] = *v
+            } else {
+                nums[i - (n -k as usize)] = *v
+            }
+
+        }
+
+    }
+
+    pub fn rotate(nums: &mut Vec<i32>, k: i32) {
+        nums.reverse();
+        nums[0..k as usize].reverse();
+        nums[k as usize..].reverse();
+    }
+
+}
+
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test] fn common_test() {
+        let mut nums = vec![1,3,5,6, 7];
+        Solution::rotate(&mut nums, 2);
+        assert_eq!(nums, vec![6, 7, 1,3,5]);
+    }
+}
